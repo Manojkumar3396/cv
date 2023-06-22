87d332c42a268bbb4c33102c3efc31eaea82285d
manoj <manojgkumar96@gmail.com>
2023-06-22 11:04:37 +0530

    manoj jun22

--- Json/app/src/main/java/com/example/json/MainActivity.java
+++ Json/app/src/main/java/com/example/json/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView=findViewById(R.id.mlistview);
+        listView=findViewById(R.id.mlistview123);
 //        textView2= (TextView) findViewById(R.id.ders);
 //         textView3= (TextView) findViewById(R.id.dersd);
 ////        TextView  textView4= (TextView) findViewById(R.id.dersg);
